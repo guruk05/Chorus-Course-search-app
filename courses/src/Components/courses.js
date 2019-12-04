7f219f94fafa57b174b39d5a9fcfb12f9c156122
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-12-04 13:50:36 +0530

    created a course component

---
+++ courses/src/Components/courses.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class Courses extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            courseData: [],
+            courses: [],
+            value: "",
+            loading: false,
+            CoursesPerPage: [75],
+            currentPage: [1],
+            indexOfAllJobs: "1709"
+          };
+    }
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
