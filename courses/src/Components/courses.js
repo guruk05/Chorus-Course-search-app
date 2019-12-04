faa2e66200ed2821252d4863a2ab63c2a0cb65d5
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-12-04 15:10:22 +0530

    importing mdb css modules

--- courses/src/Components/courses.js
+++ courses/src/Components/courses.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { MDBCol, MDBFormInline, MDBBtn } from "mdbreact";
+import 'mdbreact / dist / css / mdb.css';
 
 class Courses extends React.Component {
   constructor(props) {
