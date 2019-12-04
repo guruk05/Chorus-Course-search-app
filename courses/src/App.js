6e2790974184f7a6fc42fc13c43c6cd764fb27cb
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-12-04 13:50:14 +0530

    render Header component in parent component

--- courses/src/App.js
+++ courses/src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/header'
+import Courses from './Components/courses'
 
+class App extends React.Component{
+  constructor(props){
+    super(props)
 
+  }
+
+  render(){
+    return(
+      <Header/>
+    )
+  }
+}
 
 export default App;
