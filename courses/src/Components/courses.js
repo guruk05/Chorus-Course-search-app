412506dcd4790d98454e5cc3d90fdcacf4fb6808
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-12-04 14:26:58 +0530

    Setted the state courseData to courses

--- courses/src/Components/courses.js
+++ courses/src/Components/courses.js
@@ -1,42 +1,36 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
+class Courses extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courseData: [],
+      courses: [],
+      value: "",
+      loading: false,
+      CoursesPerPage: [75],
+      currentPage: [1],
+      indexOfAllJobs: "1709"
+    };
+  }
 
-class Courses extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            courseData: [],
-            courses: [],
-            value: "",
-            loading: false,
-            CoursesPerPage: [75],
-            currentPage: [1],
-            indexOfAllJobs: "1709"
-          };
-    }
+  componentDidMount() {
+    this.getData();
+  }
 
-    componentDidMount = async () => {
-      
-      };
-    
-      getData = async () => {
-      const url = "https://nut-case.s3.amazonaws.com/coursessc.json";
-      this.setState({ loading: true });
-      const data = await axios.get(url);
-      const courseData = await data.json();
-      const courses = courseData.data;
-      this.setState({ courseData: courses });
-      this.setState({ loading: false });
-      console.log("ComponentDidMOunt is Running");
-      }
+  getData = async () => {
+    const url = "https://nut-case.s3.amazonaws.com/coursessc.json";
+    this.setState({ loading: true });
+    const data = await axios.get(url);
+    const courseData = await data.json();
+    const courses = courseData.data;
+    this.setState({ courseData: courses });
+    this.setState({ loading: false });
+    console.log("ComponentDidMOunt is Running");
+  };
 
-
-    render(){
-        return(
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return <div>Courses</div>;
+  }
 }
