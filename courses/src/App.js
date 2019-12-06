3568e792c758bc2c22ede8fcbaa9308e72d26fb5
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-12-06 12:46:12 +0530

    Removed unused constructor

--- courses/src/App.js
+++ courses/src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import "./App.css";
 import Header from "./Components/Header/header";
 import Courses from "./Components/Courses/courses";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
